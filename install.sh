9f715149b8c5d7b77a7f2f5c477a206a185a1a47
Eric OBrien <eric@ericob.com>
2012-03-06 21:55:39 -0500

    Made everything a bit smarter. Should also now be able to see the install progress of the jdk and openssh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Quickdoop - by Eric O'Brien
 
 # Absolute path to this script, eg /home/user/bin/install.sh
 # this snippet is thanks to:
@@ -12,35 +13,69 @@ hadoop_path="/usr/local/hadoop"
 echo "Welcome to Quickdoop! This will install hadoop and configure it as a single-node cluster."
 
 echo "Installing java-6-openjdk"
-sudo apt-get -qq install openjdk-6-jdk
+echo "Sit tight, this can take a few minutes to download..."
+sudo apt-get --yes install openjdk-6-jdk
 
 echo "Installing openssh-server"
-sudo apt-get -qq install openssh-server
+sudo apt-get --yes install openssh-server
+
 
 # TODO: Allow option that doesn't add a dedicated user
-# TODO: quiet the user/group output - it gets ugly
+# TODO: don't create the group or the user if they already exist
 echo "Adding dedicated hadoop group and user"
 sudo addgroup hadoop
-sudo echo -e "\n \n \n \n \n \n" | sudo adduser --quiet --disabled-password --ingroup hadoop hduser
-sudo echo -e "hadoop\nhadoop\n" | sudo passwd --quiet hduser 
+sudo echo -e "\n \n \n \n \n \n" | sudo adduser --quiet --disabled-password --ingroup hadoop hduser > /dev/null 2>&1
+sudo echo -e "hadoop\nhadoop\n" | sudo passwd --quiet hduser > /dev/null 2>&1
 
-# TODO: check for existing keys and use those if possible
 echo "Configuring ssh keys"
-sudo su hduser -c "ssh-keygen -t rsa -P \"\" -f ~hduser/.ssh/id_rsa -q"
+if [ ! -f ~hduser/.ssh/id_rsa.pub ] # if keys don't already exist
+then
+    sudo su hduser -c "ssh-keygen -t rsa -P \"\" -f ~hduser/.ssh/id_rsa -q"
+fi
 sudo echo "StrictHostKeyChecking=no" >> ~hduser/.ssh/config
 sudo cat ~hduser/.ssh/id_rsa.pub >> ~hduser/.ssh/authorized_keys
-
 # adds self to known_hosts without user interaction
 sudo su hduser -c "ssh localhost -f \"exit\" "
 
 
-# TODO: Be able to install different versions of hadoop
-# that could get difficult if there's a different setup process
-# TODO: Make this more robust, eg if folders already exist
 echo "Installing Hadoop"
-sudo tar xzf $SCRIPTPATH/hadoop-1.0.1.tar.gz
-sudo mv $SCRIPTPATH/hadoop-1.0.1 $SCRIPTPATH/hadoop
-sudo mv $SCRIPTPATH/hadoop /usr/local/
+# if the hadoop folder already exists where we were planning to put it
+if [ -d /usr/local/hadoop ]
+then
+    echo "It looks like someone already tried to install hadoop here. Would you like to remove the old files and start with a fresh hadoop? Or should we try to continue - hoping that what's there is correct?"
+    echo "1 : start fresh (recommended)"
+    echo "2 : continue (NOT recommended)"
+    echo "ctrl-c : abort install"
+    echo "Type your choice and press Enter"
+    while read choice
+    do
+        case "$choice" in
+            1 | one | won | fresh) 
+                echo "Good choice! Starting fresh."
+                echo "Deleting old files"
+                sudo rm -rf /usr/local/hadoop
+                echo "Unzipping new files"
+                sudo tar xzf $SCRIPTPATH/hadoop-1.0.1.tar.gz
+                sudo mv $SCRIPTPATH/hadoop-1.0.1 $SCRIPTPATH/hadoop
+                echo "Moving files to /usr/local/hadoop"
+                sudo mv $SCRIPTPATH/hadoop /usr/local/
+                break ;;
+            2 | two | too | to)
+                echo "Okay... hope this works. Pretending that the hadoop files were installed correctly."
+                break ;;
+            ctrl-c) 
+                echo "That's not exactly what I meant, but okay. Aborting install."
+                exit 1 ;;
+            *) echo "Invalid input, try again." ;;
+        esac
+    done
+else
+    echo "Unzipping files"
+    sudo tar xzf $SCRIPTPATH/hadoop-1.0.1.tar.gz
+    sudo mv $SCRIPTPATH/hadoop-1.0.1 $SCRIPTPATH/hadoop
+    echo "Moving files to /usr/local/hadoop"
+    sudo mv $SCRIPTPATH/hadoop /usr/local/
+fi
 
 # now we can start using $hadoop_path
 # TODO: don't add the lines if they already exist
@@ -57,3 +92,5 @@ sudo cat $SCRIPTPATH/modfiles/hdfs-site.xml > $hadoop_path/conf/hdfs-site.xml
 sudo mkdir -p /app/hadoop/tmp
 sudo chown hduser:hadoop /app/hadoop/tmp
 sudo chmod 750 /app/hadoop/tmp
+
+echo "Done! Try it out by logging in to hduser and using the \"hadoop\" command."
